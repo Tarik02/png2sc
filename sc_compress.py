4066ac7ed61a46736ad3d8d8f1f3dcf580fc6aac
Xset-s <greensmoked@yandex.com>
2017-03-25 18:40:08 +0300

    Version 1.0.4

--- sc_compress.py
+++ sc_compress.py
@@ -1,13 +1,17 @@
 # coding: utf-8
-# Author: Xset
 
+# Author: Xset
+# Version: 1.0.4
+# Last updated: 2017-03-24
+
+import re
 import os
-import lzma
-import math
+import sys
 import struct
 import sqlite3
-import urllib3
 import binascii
+import urllib3
+import subprocess
 
 from PIL import Image,  ImageDraw
 from modules import *
@@ -15,13 +19,57 @@ from modules import *
 compileDir = './compile/'
 compiledDir = './compiled/'
 
-# Set OFF SSL warnings (because we will download from github)
+findFiles = {}
+picCount = 0
+
+# Database connection
+dbcon = sqlite3.connect("PixelData.db")
+dbcon.isolation_level = None
+dbcur = dbcon.cursor()
+dbcon.row_factory = sqlite3.Row
+dbcon.text_factory = str
+
 urllib3.disable_warnings()
 
-toCompile = os.listdir(compileDir)
+# Special class
+class BytesWriter:
+    
+    def __init__(self, fileName):
+        self.fileName = fileName
+        self.handler = None
+    
+    def WStart(self):
+        self.handler = open(self.fileName, "wb+")
 
-def _(message):
-    print(u"[RELEASE] %s" % message)
+    def WStop(self):
+        self.handler.close()
+
+    def WBytes(self, value):
+        self.handler.write(bytes)
+
+    def WByte(self, value):
+        self.handler.write(struct.pack('<b', int(value)))
+
+    def WUnsignedByte(self, value):
+        self.handler.write(struct.pack('<B', int(value)))
+
+    def W4Bytes(self, b, bb, bbb, bbbb):
+        self.handler.write(struct.pack('4B', b, bb, bbb, bbbb))
+
+    def WShort(self, value):
+        self.handler.write(struct.pack('<h', int(value)))
+
+    def WUnsignedShort(self, value):
+        self.handler.write(struct.pack('<H', int(value)))
+
+    def WInt(self, value):
+        self.handler.write(struct.pack('<i', int(value)))
+
+    def WUnsignedInt(self, value):
+        self.handler.write(struct.pack('<I', int(value)))
+
+    def WBool(self, value):
+        self.handler.write(struct.pack('<?', int(value)))
 
 # Latest database
 def downloadDB():
@@ -31,20 +79,13 @@ def downloadDB():
     response = http.request('GET', 'http://github.com/Xset-s/png2sc/raw/master/PixelData.db')
     database = response.data
     _('The latest database successfully downloaded.' + "\n")
- 
+
     dbFile = open("PixelData.db", "wb+")
     dbFile.write(database)
     dbFile.close()
 
 if not os.path.exists('PixelData.db'):
     downloadDB()
-
-# Database connection
-dbcon = sqlite3.connect("PixelData.db")
-dbcon.isolation_level = None
-dbcur = dbcon.cursor()
-dbcon.row_factory = sqlite3.Row
-dbcon.text_factory = str
 
 def checkAlreadyExists(filename):
     dbcur.execute('select * from PixelType where filename = ?', [filename])
@@ -55,106 +96,234 @@ def checkAlreadyExists(filename):
     
     return rrf
 
-def float2int(flt):
-    return int(math.ceil(flt))
+# Useful functions :)
+def file2bytes(fileName):
+    with open(fileName, "rb+") as handle:
+        bytes = handle.read()
 
-for file in toCompile:
-    baseName, ext = os.path.splitext(os.path.basename(file))
-    file = compileDir + file
-    if file.endswith('png'):
-        _('FileName: ' + file)
-        image = Image.open(file)
-        
-        width  = image.size[0]
-        height = image.size[1]
+    return bytes
 
-        _('Width: ' + str(width))
-        _('Height: ' + str(height))
+def bytes2file(bytes, fileName):
+    handler = open(fileName, "wb+")
+    handler.write(bytes)
+    handler.close()
+
+def writeImage(file, baseName, fileName):
+    _("Collecting information...")
+
+    # Open image
+    image = Image.open(file)
+    data = checkAlreadyExists(baseName)
+
+    # Get data from database
+    if not data is None:
+        fileType = data[1]
+        subType = data[2]
+    else:
+        _("Sorry, we can\'t find this texture in out database... May be you changed filename?")
+        _("We will use standart fileType, subType and headerBytes. (1, 0, None)" + "\n")
         
+        fileType = 1
+        subType = 0
+    
+    # Height and width
+    width  = image.size[0]
+    height = image.size[1]
+
+    pixels = []
+    iSrcPix = 0
+
+    # BPixelSize
+    if subType == 0:
         BFPXFormat = 4
-
-        # Size
+    elif subType == 2 or subType == 4 or subType == 6:
+        BFPXFormat = 2
+    else:
+        _("Unknown pixel type %s" % (subType))
+        sys.exit(0)
+        
+    if BFPXFormat:
         packetSize = ((width) * (height) * BFPXFormat) + 5;
-        _('PacketSize: ' + str(packetSize) + "\n")
-
-        # Get data from database
-        data = checkAlreadyExists(baseName)
-        
-        if not data is None:
-            subType = data[1]
-            headerBytes = binascii.unhexlify(data[2])
-        else:
-            _('Sorry, we can\'t find this texture in out database... May be you changed filename?')
-            _('We will use standart subType and headerBytes. (0, None)' + "\n")
-
-            subType = 0
-            headerBytes = b''
-            
-        # Create new packet
-        p = ByteArray()
-
-        # Формируем пакет
-        p.writeByte(1)
-        p.writeUnsignedInt(packetSize)
-        p.writeByte(subType)
-        p.writeUnsignedShort(width)
-        p.writeUnsignedShort(height)
-        
-        data = p.toByteArray()
-
-        # Temp file
-        cModule = open("temp.tex_sc", "wb+")
-        cModule.write(data)
-        
-        _( 'Saving _tex.sc...' )
-        
+    
+    _("About: fileType %s, fileSize: %s, subType: %s, width: %s, height: %s" % (fileType, packetSize, subType, width, height) + "\n")
+    
+    imgl = image.load()
+    
+    if fileType == 28 or fileType == 27:
         for y in range(0, height):
             for x in range(0, width):
-
-                # Get Pixel
                 c = image.getpixel((x, y))
-                    
-                r = c[0]
-                g = c[1]
-                b = c[2]
-                a = c[3]
+                pixels.append(c)
 
-                # Write pixel in file
-                if subType == 0:                    
-                    cModule.write(struct.pack('4B', r, g, b, a))
-                # RGB4444
-                elif subType == 2:
-                    pack = 0
-                    
-                    pack += int(a / 0x11) 
-                    pack += int(b / 0x11) << 4                         
-                    pack += int(g / 0x11) << 8
-                    pack += int(r / 0x11) << 12
+        for l in range(int(height / 32)):
+            for k in range(int(width / 32)):
+                for j in range(32):
+                    for h in range(32):
+                        pixels[iSrcPix] = imgl[h + (k * 32), j + (l * 32)]
+                        iSrcPix += 1
+                        
+            for j in range(32):
+                for h in range(width % 32):
+                    pixels[iSrcPix] = imgl[h + (width - (width % 32)), j + (l * 32)]
+                    iSrcPix += 1
 
-                    cModule.write(struct.pack('<H', val))
-                    
-        cModule.close()
+        for k in range(int(width / 32)):
+            for j in range(int(height % 32)):
+                for h in range(32):
+                    pixels[iSrcPix] = imgl[h + (k * 32), j + (height - (height % 32))]
+                    iSrcPix += 1
 
-        _( 'Compressing data...' )
-        os.system('lzma.exe e temp.tex_sc temp_.tex_sc')
-        _( 'Compressing OK...' )
+        for j in range(height % 32):
+            for h in range(width % 32):
+                pixels[iSrcPix] = imgl[h + (width - (width % 32)), j + (height - (height % 32))]
+                iSrcPix += 1
 
-        os.remove('temp.tex_sc')
-        os.rename('temp_.tex_sc', 'temp.tex_sc')
+    image.putdata(pixels)
+    
+    # Create new packet
+    p = BytesWriter(fileName)
+
+    # Start Handler
+    p.WStart()
+
+    # Packet
+    p.WByte(28)
+    p.WUnsignedInt(packetSize)
+    p.WByte(subType)
+    p.WUnsignedShort(width)
+    p.WUnsignedShort(height)
+
+    for y in range(0, height):
+        for x in range(0, width):
+
+            # Get Pixel
+            c = image.getpixel((x, y))
+                
+            r = c[0]
+            g = c[1]
+            b = c[2]
+            a = c[3]
+
+            if subType == 0:
+                p.W4Bytes(r, g, b, a)
+                
+            elif subType == 2: # RGB4444 to RGB8888
+                val = 0
+                
+                val += int(a / 0x11) 
+                val += int(b / 0x11) << 4                         
+                val += int(g / 0x11) << 8
+                val += int(r / 0x11) << 12
+
+                p.WUnsignedShort(val)
+                
+            elif subType == 4: # RGB565 to RGB8888
+                val = 0
+                
+                blue = (b >> 3)
+                green = (g >> 2) << 5
+                red = (r >> 3) << 11
+
+                val = blue + green + red
+                p.WUnsignedShort(val)
+
+            elif subType == 6: # RGB555 to RGB8888
+                red = r << 16
+                green = g << 9
+                blue = b << 6
+                alpha = a << 3
+
+                val = red | green | blue | alpha
+                
+                p.WUnsignedShort(val)
+
+    if fileName.endswith("__.tex.sc"):
+        # End :c
+        p.WByte(0)
+        p.WByte(0)
+        p.WByte(0)
+        p.WByte(0)
+        p.WByte(0)
+
+    p.WStop()
+
+def compressLZMA(fileName):
+    _("Saving as sprite...")
+    
+    result = subprocess.run(["lzma.exe", "e", "temp_tex.sc", "temp_.tex_sc"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    try:
+        result.check_returncode()
+    except:
+        _("ERROR. Check lzma.exe")
+    
+    os.remove("temp_tex.sc")
+    os.rename("temp_.tex_sc", "temp_tex.sc")
+    
+    # Module with LZMA
+    with open("temp_tex.sc", "rb") as lz:
+        lzModule = lz.read()
+        lzModule = lzModule[0:9] + lzModule[13:]
+
+        mModule = open("./compiled/" + re.sub("\_*tex\_*", "_tex.sc", fileName), "wb+")
+        mModule.write(lzModule)
+        mModule.close()
         
-        # Module with LZMA
-        with open('temp.tex_sc', 'rb') as lz:
-            lzModule = lz.read()
-            lzModule = lzModule[0:9] + lzModule[13:]
+        lz.close()
+        os.remove("temp_tex.sc")
 
-            # Add headers
-            lzModule = headerBytes + lzModule
+        _("Saving completed" + "\n")
 
-            mModule = open('./compiled/' + baseName + '.sc', "wb+")
-            mModule.write(lzModule)
-            mModule.close()
-            _( 'Saving OK...' )            
+# Parse all files from dir
+def generateFilesList(dir):
+    if findFiles.get(dir) is None:
+        findFiles[dir] = []
+                
+    toCompile = os.listdir(dir)
 
-            lz.close()
+    for file in toCompile:
+        fullname = dir + file
+        if os.path.isdir(fullname):
+            dir_ = fullname + "/"
+            
+            if findFiles.get(dir_) is None:
+                findFiles[dir_] = []
+                
+            generateFilesList(dir_)
+        else:
+            if file.endswith("png"):
+                findFiles[dir].append(file)
 
-            os.remove("temp.tex_sc")
+generateFilesList(compileDir)
+
+for dirName in findFiles:
+    for file in findFiles[dirName]:
+        file = dirName + file
+        baseName, ext = os.path.splitext(os.path.basename(file))
+        
+        # If we will compile 2 files in one
+        if not dirName == compileDir:            
+            fileName = "temp_" + ("_" * picCount) + "tex.sc"
+            writeImage(file, baseName, fileName)
+
+            picCount += 1
+
+            if picCount == len(findFiles[dirName]):
+                b_bytes = b''
+                
+                for j in range(picCount):
+                    f_name = "temp_" + ("_" * j) + "tex.sc"
+                    b_bytes += file2bytes(f_name)
+                    os.remove(f_name)
+                    
+                bytes2file(b_bytes, "temp_tex.sc")
+                compressLZMA(baseName)
+                    
+        # if we have standart file (1 file)
+        elif file.endswith("png"):
+            
+            fileName = "temp_tex.sc"
+            writeImage(file, writeImage, fileName)
+
+            _(baseName)
+            compressLZMA(baseName)
+            os.remove("temp.tex.sc")
--- sc_decompress.py
+++ sc_decompress.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+
 # Original from https://github.com/royale-proxy/
 # Rewritten by Xset
 
@@ -15,30 +16,11 @@ from modules import *
 from PIL import Image
 
 # Folders
-folder = './decompile/'
-folder_export = './decompiled/'
+folder = "./decompile/"
+folder_export = "./decompiled/"
 
-# Set OFF SSL warnings (because we will download from github)
+# Disable warnings (ssl)
 urllib3.disable_warnings()
-
-def _(message):
-    print(u"[RELEASE] %s" % message)
-
-# Latest database
-def downloadDB():
-    http = urllib3.PoolManager()
-    
-    _('Downloading the latest database...')
-    response = http.request('GET', 'http://github.com/Xset-s/png2sc/raw/master/PixelData.db')
-    database = response.data
-    _('The latest database successfully downloaded.' + "\n")
-
-    dbFile = open("PixelData.db", "wb+")
-    dbFile.write(database)
-    dbFile.close()
-
-if not os.path.exists('PixelData.db'):
-    downloadDB()
 
 # Database connection
 dbcon = sqlite3.connect("PixelData.db")
@@ -47,8 +29,21 @@ dbcur = dbcon.cursor()
 dbcon.row_factory = sqlite3.Row
 dbcon.text_factory = str
 
+# Latest database
+def downloadDB():
+    http = urllib3.PoolManager()
+    
+    _("Downloading the latest database...")
+    response = http.request("GET", "http://github.com/Xset-s/png2sc/raw/master/PixelData.db")
+    database = response.data
+    _("The latest database successfully downloaded." + "\n")
+
+    dbFile = open("PixelData.db", "wb+")
+    dbFile.write(database)
+    dbFile.close()
+
 def checkAlreadyExists(filename):
-    dbcur.execute('select * from PixelType where filename = ?', [filename])
+    dbcur.execute("select * from PixelType where filename = ?", [filename])
     rrf = dbcur.fetchone()
     
     if rrf is None:
@@ -59,20 +54,23 @@ def checkAlreadyExists(filename):
 def convert_pixel(pixel, type):
     if type == 0:
         # RGB8888
-        return struct.unpack('4B', pixel)
+        return struct.unpack("4B", pixel)
     elif type == 2:
         # RGB4444        
-        pixel, = struct.unpack('<H', pixel)
+        pixel, = struct.unpack("<H", pixel)
         #print(pixel)
         return (((pixel >> 12) & 0xF) << 4, ((pixel >> 8) & 0xF) << 4,
                 ((pixel >> 4) & 0xF) << 4, ((pixel >> 0) & 0xF) << 4)
     elif type == 4:
         # RGB565
         pixel, = struct.unpack("<H", pixel)
+        #_(pixel)
+        #_([(((pixel >> 11) & 0x1F) << 3, ((pixel >> 5) & 0x3F) << 2, (pixel & 0x1F) << 3)])
         return (((pixel >> 11) & 0x1F) << 3, ((pixel >> 5) & 0x3F) << 2, (pixel & 0x1F) << 3)
     elif type == 6:
         # RGB555?
         pixel, = struct.unpack("<H", pixel)
+        
         return ((pixel >> 16) & 0x80, (pixel >> 9) & 0x7C,
                 (pixel >> 6) & 0x3E, (pixel >> 3) & 0x1F)
     elif type == 10:
@@ -80,38 +78,41 @@ def convert_pixel(pixel, type):
         pixel, = struct.unpack("<B", pixel)
         return ((pixel) & 0x3, ((pixel >> 2) & 0x7) << 2, ((pixel >> 5) & 0x7) << 5)
     else:
-        raise Exception("Unknown pixel type {}.".format(type))
+        raise Exception("Unknown pixel type %s." % (type))
+
+if not os.path.exists("PixelData.db"):
+    downloadDB()
 
 files = os.listdir(folder)
 for file_d in files:
     baseName, ext = os.path.splitext(os.path.basename(file_d))
-    if file_d.endswith('_tex.sc'):
-        with open(folder + file_d, 'rb') as fh:
+    if file_d.endswith("_tex.sc"):
+        with open(folder + file_d, "rb") as fh:
             data = fh.read()
-            
-            # Header
-            header = data[:26]
-            
-            data = data[26:]
-            data = data[0:9] + (b'\x00' * 4) + data[9:]
+
+            # Fix headers
+            if data[0] != 93:
+                data = data[26:]
+                
+            data = data[0:9] + (b"\x00" * 4) + data[9:]
             
             # LZMA Decompression
-            _( 'Decompressing data...' )
+            _("Decompressing data...")
             decompressed = lzma.LZMADecompressor().decompress(data)
-            _( 'Decompressing OK' + "\n" )
+            _("Decompressing OK" + "\n")
             
-            _( 'Creating picture...' )
             i = 0
             picCount = 0
 
             p = ByteArray(decompressed)
-            
-            while len(decompressed[i:]) > 5:
+
+            _("Collecting information...")
+            while len(decompressed[i:]) > 5:        
                 fileType = decompressed[i]
-                fileSize, = struct.unpack('<I', decompressed[i + 1:i + 5])
+                fileSize, = struct.unpack("<I", decompressed[i + 1:i + 5])
                 subType = decompressed[i + 5]
-                width, = struct.unpack('<H', decompressed[i + 6:i + 8])
-                height, = struct.unpack('<H', decompressed[i + 8:i + 10])
+                width, = struct.unpack("<H", decompressed[i + 6:i + 8])
+                height, = struct.unpack("<H", decompressed[i + 8:i + 10])
                 
                 i += 10
                 
@@ -123,20 +124,15 @@ for file_d in files:
                     pixelSize = 1
                 else:
                     raise Exception("Unknown pixel type {}.".format(subType))
-                
-                if checkAlreadyExists(baseName) is None:
-                    _('Writing data in database...')
-                    dbcur.execute('INSERT INTO PixelType (filename, pixel, hexsc) values (?, ?, ?)', [baseName, subType, binascii.hexlify(header)])
-                    _('Data successfully written' + "\n")
 
-                _('About: fileName {}, fileType {}, fileSize: {}, subType: {}, width: {}, height: {}'.format(file_d, fileType, fileSize, subType, width, height))
-                
+                _("About: fileName %s, fileType %s, fileSize: %s, subType: %s, width: %s, height: %s" % (file_d, fileType, fileSize, subType, width, height))
+
+                _("Creating picture...")
                 img = Image.new("RGBA", (width, height))
                 
                 pixels = []
                 for y in range(height):
                     for x in range(width):
-                        #print(str(convert_pixel(decompressed[i:i + pixelSize], subType)))
                         pixels.append(convert_pixel(decompressed[i:i + pixelSize], subType))
                         i += pixelSize
                 
@@ -167,8 +163,15 @@ for file_d in files:
                         for h in range(width % 32):
                             imgl[h + (width - (width % 32)), j + (height - (height % 32))] = pixels[iSrcPix]
                             iSrcPix += 1
-                
-                _( "Saving as png..." )
-                img.save(folder_export + baseName + ('_' * picCount) + '.png', 'PNG')
+                            
+                fullname = baseName + ('_' * picCount)
+
+                if checkAlreadyExists(fullname) is None:
+                    _("Writing data in database...")
+                    dbcur.execute('INSERT INTO PixelType (filename, fileType, pixel, hexsc) values (?, ?, ?, ?)', [fullname, fileType, subType, ''])
+                    _("Data successfully written" + "\n")
+                    
+                _("Saving as png...")
+                img.save(folder_export + fullname + ".png", "PNG")
                 picCount += 1
-                _( "Saving completed\n" )
+                _("Saving completed" + "\n")
